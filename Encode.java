5af60ad0001cf4e5f9f92972c996e038da9f21f0
Josephine <mexy1991@outlook.com>
2019-04-24 11:55:11 +0200

    Create Encode.java

---
+++ Encode.java
@@ -0,0 +1,11 @@
+/* * * * * * * * * * * * * * 
+* Forfattere:
+* Josephine Søgaard Andersen, josea18@student.sdu.dk
+* Josias Kure, joulr18@student.sdu.dk 
+* Kasper Jonassen, kajon18@student.sdu.dk 
+* * * * * * * * * * * * * */
+
+public class Encode{
+	
+
+}
